7bbf116ffa88d3ca859a2c77f226ef960b6440ed
Junhao Zhang <junhaodev@gmail.com>
2021-03-08 10:20:40 -0600

    Added spawning trees

---
+++ Assets/Scripts/WarningSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class WarningSpawner : MonoBehaviour
+{
+    public GameObject prefab;
+    public Vector3 offset;
+
+    public float tick = 0;
+    public float maxTick = 0;
+
+    private void Update()
+    {
+        tick += Time.deltaTime;
+
+        if (tick >= maxTick)
+        {
+            tick = 0;
+            Vector3 position = new Vector3(Random.Range(-28, 28), 0, Random.Range(-28, 28));
+            Instantiate(prefab, position + offset, Quaternion.identity);
+        }
+    }
+}
