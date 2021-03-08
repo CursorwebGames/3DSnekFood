71577cb471ddc67ef4cfc8a1450ac29e04d02dad
Junhao Zhang <junhaodev@gmail.com>
2021-03-07 21:08:18 -0600

    Added SNEK AI

---
+++ Assets/Scripts/HeadFollower.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class HeadFollower : MonoBehaviour
+{
+    public float speed = 200;
+    public Transform head;
+
+    void Update()
+    {
+        Vector3 direction = head.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.up);
+        transform.localRotation *= rotation;
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/HeadManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class HeadManager : MonoBehaviour
+{
+    public GameObject prefab;
+    public Transform tail;
+       
+    public float tickFrequency = 100;
+    public float tick = 0;
+
+    public Transform apple;
+    public NavMeshAgent nav;
+
+
+    private void Update()
+    {
+        tick += Time.deltaTime;
+        if (tick >= tickFrequency)
+        {
+            AddSegment();
+            tick = 0;
+        }
+
+        nav.SetDestination(apple.position);
+    }
+
+    private void AddSegment()
+    {
+        GameObject segment = Instantiate(prefab, tail.position, Quaternion.identity);
+        HeadFollower comp = segment.GetComponent<HeadFollower>();
+        comp.head = tail;
+        tail = segment.transform;
+    }
+}
