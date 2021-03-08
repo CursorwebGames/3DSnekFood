372a585f9519c63b05faddb13d731d45b64db70a
Junhao Zhang <junhaodev@gmail.com>
2021-03-08 09:50:44 -0600

    Added warning indicator

---
+++ Assets/Scripts/TreeSpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class TreeSpawner : MonoBehaviour
+{
+    public GameObject prefab;
+    public Vector3 offset;
+
+    public float tick;
+    public float maxTick;
+
+    private void Update()
+    {
+        tick += Time.deltaTime;
+
+        if (tick >= maxTick)
+        {
+            Destroy(gameObject);
+            Instantiate(prefab, transform.position + offset, Quaternion.identity);
+        }
+    }
+}
