506d9f4569b9a251a139b5470f7d1a3c5a1d0e69
Junhao Zhang <junhaodev@gmail.com>
2021-03-09 10:37:02 -0600

    Added score

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Text scoreText;
     public bool isEnd = false;
+
+    public float tick = 0;
+    public float maxTick = 5;
+    private int score = 0;
+
 
     private void Update()
     {
         if (!isEnd) Cursor.lockState = CursorLockMode.Locked;
         else Cursor.lockState = CursorLockMode.None;
+
+        tick += Time.deltaTime;
+        if (tick >= maxTick)
+        {
+            tick = 0;
+            score++;
+            scoreText.text = score.ToString();
+        }
     }
 }
